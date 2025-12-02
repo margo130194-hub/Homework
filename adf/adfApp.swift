1bce4e17f5f686206cae2a1b264897e488471655
Margarita Matsonko <margo130194@gmail.com>
2025-12-02 11:41:21 +0100

    Initial Commit

---
+++ adf/adfApp.swift
@@ -0,0 +1,17 @@
+//
+//  adfApp.swift
+//  adf
+//
+//  Created by Margarita Matsonko on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct adfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
