a0ab78707821d42352461b3d727ede4ef81d97d7
Margarita Matsonko <margo130194@gmail.com>
2026-03-25 09:39:29 +0100

    new homework

---
+++ Homework 24/Homework 24/CityModel.swift
@@ -0,0 +1,13 @@
+//
+//  CityModel.swift
+//  Homework 24
+//
+//  Created by Margarita Matsonko on 22/03/2026.
+//
+
+import UIKit
+
+struct CityModel{
+    let name: String
+    let timeZone: TimeZone
+}
